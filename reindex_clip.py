8c3a8943aa9ed472cb821fb486615238a93f264b
Sumit sharma <sumibaman@gmail.com>
2026-01-23 01:35:49 +0530

    Refactor reindex_clip.py to improve structure

--- reindex_clip.py
+++ reindex_clip.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 # --- CONFIGURATION ---
 QDRANT_URL = "https://01316258-dac8-44a6-9cd3-da472e9d12e6.us-east4-0.gcp.cloud.qdrant.io"
-QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.ZrhpQ1sheNE2d4lhadPSOiw5VmYLN9I1b4U58u-5TLU"# <--- PASTE KEY
+QDRANT_API_KEY = ""# <--- PASTE KEY
 COLLECTION_NAME = "healthcare_clip"  # <--- New Collection Name
 
 # PATHS
@@ -79,4 +79,5 @@ def run_upload():
     print("🎉 CLIP Upload Complete! Your database is now Text-Smart.")
 
 if __name__ == "__main__":
+
     run_upload()
